45c94c6b5c9a6706f0f778bc4b728d915349f346
EunOh <bdohhhhh@gmail.com>
2025-07-19 20:26:41 +0900

    [add]: test-ui

---
+++ app/(public)/test-page/page.tsx
@@ -0,0 +1,12 @@
+import PostLayout from "@/features/posts/ui/layout";
+import TestUi from "@/features/posts/ui/test-ui";
+
+function TestPage() {
+  return (
+    <PostLayout>
+      <TestUi />
+    </PostLayout>
+  );
+}
+
+export default TestPage;
---
+++ src/features/posts/ui/test-ui.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import UploadThingButton from "@/components/ui/upload-button";
+
+function TestUi() {
+  return <UploadThingButton onUploadComplete={() => {}} />;
+}
+
+export default TestUi;
