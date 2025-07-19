e6e4137140ec2522dfb2466eccc4bb990a03b00a
EunOh <bdohhhhh@gmail.com>
2025-07-19 20:29:19 +0900

    [update]: queries

--- src/features/posts/queries.ts
+++ src/features/posts/queries.ts
@@ -1,11 +1,21 @@
 import { QUERY_KEYS } from "@/consts/constants";
 import { Post } from "@/generated/prisma";
-import { useQuery } from "@tanstack/react-query";
-import { getPosts } from "./apis";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { createPost, getPosts } from "./apis";
 
 export function usePostsQuery() {
   return useQuery<Post[], Error, Post[]>({
     queryKey: [QUERY_KEYS.POSTS],
     queryFn: () => getPosts(false),
+  });
+}
+
+export function useCreatePostMutation() {
+  const queryClient = useQueryClient();
+  return useMutation<Post, Error, FormData>({
+    mutationFn: (formData) => createPost(formData),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.POSTS] });
+    },
   });
 }
