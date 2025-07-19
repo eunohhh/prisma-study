159bc9c23b13a741eafff54741ca4bcd328fe100
EunOh <bdohhhhh@gmail.com>
2025-07-19 20:29:13 +0900

    [update]: posts.tsx

--- src/features/posts/ui/posts.tsx
+++ src/features/posts/ui/posts.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useEffect } from "react";
 import { usePostsQuery } from "../queries";
 import { Post } from "./post";
@@ -12,7 +14,7 @@ function Posts() {
   if (isPending) return <div>Loading...</div>;
 
   return (
-    <ol className="p-12 md:p-8 text-black">
+    <ol className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-12 md:p-8 text-black">
       {posts?.map((post, itemNo) => (
         <li key={post.id} className="mb-4">
           <Post
