7990e0704d76e05ee9c14e9d40a283f157550e78
EunOh <bdohhhhh@gmail.com>
2025-07-19 20:27:11 +0900

    [update]: api/posts

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,6 +1,34 @@
 import prisma from "@/lib/db";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET() {
   const posts = await prisma.post.findMany();
-  return Response.json(posts, { status: 200 });
+  return NextResponse.json(posts, { status: 200 });
+}
+
+export async function POST(request: NextRequest) {
+  const formDataObject = await request.formData();
+  const title = formDataObject.get("title") as string;
+  const url = formDataObject.get("url") as string;
+  const content = formDataObject.get("content") as string;
+
+  if (!title || !url || !content) {
+    return NextResponse.json(
+      { error: "Missing required fields" },
+      { status: 400 }
+    );
+  }
+
+  const post = await prisma.post.create({
+    data: { title, url, content, vote: 0, createdAt: new Date() },
+  });
+
+  console.log(post);
+
+  revalidatePath("/");
+  redirect("/");
+
+  // return NextResponse.json(post, { status: 201 });
 }
