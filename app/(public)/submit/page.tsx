ec8152e0452b2d17297548362a9c3a91598d57d3
EunOh <bdohhhhh@gmail.com>
2025-07-19 20:26:30 +0900

    [add]: submit page

---
+++ app/(public)/submit/page.tsx
@@ -0,0 +1,12 @@
+import PostLayout from "@/features/posts/ui/layout";
+import { PostForm } from "@/features/posts/ui/post-submit-form";
+
+function PostSubmitPage() {
+  return (
+    <PostLayout>
+      <PostForm />
+    </PostLayout>
+  );
+}
+
+export default PostSubmitPage;
