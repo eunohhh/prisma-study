90f3b969914ed71e9dc2a2b90ef6cf31e43e83b6
Eun Oh <bdohhhhh@gmail.com>
2025-07-20 12:50:09 +0900

    [update]: page.tsx - 빌드 타임아웃 에러 수정 시도6

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,7 +1,9 @@
 import { LIMIT, QUERY_KEYS } from "@/consts/constants";
 import { getPaginatedPosts } from "@/features/posts/apis";
+import { PostInfiniteQuery } from "@/features/posts/types";
 import PostLayout from "@/features/posts/ui/layout";
 import Posts from "@/features/posts/ui/posts";
+import { Post } from "@/generated/prisma";
 import {
   dehydrate,
   HydrationBoundary,
@@ -16,32 +18,31 @@ async function PostPage() {
   //   queryFn: () => getPosts(true),
   //   staleTime: 0,
   // });
-  try {
-    await queryClient.prefetchQuery({
-      queryKey: [QUERY_KEYS.POSTS, 1],
-      queryFn: () => getPaginatedPosts(true, 1, LIMIT),
-      staleTime: 1000 * 60 * 5,
-    });
-  } catch (error) {
-    console.log(error);
-  }
+  // try {
+  //   await queryClient.prefetchQuery({
+  //     queryKey: [QUERY_KEYS.POSTS, 1],
+  //     queryFn: () => getPaginatedPosts(true, 1, LIMIT),
+  //     staleTime: 1000 * 60 * 5,
+  //   });
+  // } catch (error) {
+  //   console.log(error);
+  // }
 
-  // await queryClient.prefetchInfiniteQuery<
-  //   PostInfiniteQuery,
-  //   Error,
-  //   Post[],
-  //   [string],
-  //   number
-  // >({
-  //   queryKey: [QUERY_KEYS.POSTS],
-  //   initialPageParam: 1,
-  //   getNextPageParam: (lastPage, pages) => {
-  //     return lastPage.totalPages > pages.length ? pages.length + 1 : undefined;
-  //   },
-  //   queryFn: ({ pageParam }) => getPaginatedPosts(true, pageParam, LIMIT),
-  //   pages: 1,
-  //   staleTime: 0,
-  // });
+  await queryClient.prefetchInfiniteQuery<
+    PostInfiniteQuery,
+    Error,
+    Post[],
+    [string],
+    number
+  >({
+    queryKey: [QUERY_KEYS.POSTS],
+    initialPageParam: 1,
+    getNextPageParam: (lastPage, pages) => {
+      return lastPage.totalPages > pages.length ? pages.length + 1 : undefined;
+    },
+    queryFn: ({ pageParam }) => getPaginatedPosts(true, pageParam, LIMIT),
+    pages: 1,
+  });
 
   const dehydratedState = dehydrate(queryClient);
 
