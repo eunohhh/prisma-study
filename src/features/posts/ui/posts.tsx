b5d6f591ae80e27ecffa3af36376e1ebab36712a
Eun Oh <bdohhhhh@gmail.com>
2025-07-20 11:32:35 +0900

    [update]: posts.tsx

--- src/features/posts/ui/posts.tsx
+++ src/features/posts/ui/posts.tsx
@@ -1,31 +1,43 @@
 "use client";
 
 import { useEffect } from "react";
-import { usePostsQuery } from "../queries";
+import { usePostsInfiniteQuery } from "../queries";
+import InfiniteScroll from "./infinite-scroll";
 import { Post } from "./post";
 
 function Posts() {
-  const { data: posts, isPending, error } = usePostsQuery();
+  const {
+    data: posts,
+    isPending,
+    error,
+    fetchNextPage,
+    hasNextPage,
+  } = usePostsInfiniteQuery();
+
+  useEffect(() => {
+    console.log(hasNextPage);
+  }, [hasNextPage]);
 
   useEffect(() => {
     if (error) console.error(error);
   }, [error]);
 
-  if (isPending) return <div>Loading...</div>;
-
   return (
     <ol className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-12 md:p-8 text-black">
-      {posts?.map((post, itemNo) => (
-        <li key={post.id} className="mb-4">
-          <Post
-            id={post.id}
-            itemNo={itemNo + 1}
-            title={post.title}
-            votes={post.vote}
-            url={post.url}
-          />
-        </li>
-      ))}
+      <InfiniteScroll fetchNextPage={fetchNextPage} hasNextPage={hasNextPage}>
+        {posts?.map((post, itemNo) => (
+          <li key={post.id} className="mb-4">
+            <Post
+              id={post.id}
+              itemNo={itemNo + 1}
+              title={post.title}
+              votes={post.vote}
+              url={post.url}
+            />
+          </li>
+        ))}
+        {isPending && <div>Loading...</div>}
+      </InfiniteScroll>
     </ol>
   );
 }
