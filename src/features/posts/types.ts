e4ec98a21d02c197c240b6406900747d1f2e33c4
Eun Oh <bdohhhhh@gmail.com>
2025-07-20 11:32:53 +0900

    [update]: types

--- src/features/posts/types.ts
+++ src/features/posts/types.ts
@@ -1,4 +1,12 @@
+import { Post } from "@prisma/client";
 import { z } from "zod";
 import { postFormSchema } from "./schema";
 
 export type PostFormValues = z.infer<typeof postFormSchema>;
+
+export type PostInfiniteQuery = {
+  data: Post[];
+  totalItems: number;
+  totalPages: number;
+  currentPage: number;
+};
