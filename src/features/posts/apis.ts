e5ffd7fdcff35f166f5a20aa3fc0148be0c74552
Eun Oh <bdohhhhh@gmail.com>
2025-07-20 11:32:42 +0900

    [update]: apis

--- src/features/posts/apis.ts
+++ src/features/posts/apis.ts
@@ -1,6 +1,7 @@
 import { Post } from "@/generated/prisma";
 import api from "@/lib/axios";
 import serverApi from "@/lib/axios-server";
+import { PostInfiniteQuery } from "./types";
 
 export function getPosts(isServer: boolean) {
   return isServer
@@ -10,4 +11,22 @@ export function getPosts(isServer: boolean) {
 
 export function createPost(formData: FormData) {
   return api.post<Post, Post>("/api/posts", formData);
+}
+
+export function getPost(id: string) {
+  return api.get<Post, Post>(`/api/posts/${id}`);
+}
+
+export function getPaginatedPosts(
+  isServer: boolean,
+  page: number,
+  limit: number
+) {
+  return isServer
+    ? serverApi.get<PostInfiniteQuery, PostInfiniteQuery>(
+        `/api/posts?page=${page}&limit=${limit}`
+      )
+    : api.get<PostInfiniteQuery, PostInfiniteQuery>(
+        `/api/posts?page=${page}&limit=${limit}`
+      );
 }
