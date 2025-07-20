3a6711ef9f9021b4bd22110e07ace00aede60c63
Eun Oh <bdohhhhh@gmail.com>
2025-07-20 11:32:48 +0900

    [update]: queries

--- src/features/posts/queries.ts
+++ src/features/posts/queries.ts
@@ -1,7 +1,13 @@
-import { QUERY_KEYS } from "@/consts/constants";
+import { LIMIT, QUERY_KEYS } from "@/consts/constants";
 import { Post } from "@/generated/prisma";
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { createPost, getPosts } from "./apis";
+import {
+  useInfiniteQuery,
+  useMutation,
+  useQuery,
+  useQueryClient,
+} from "@tanstack/react-query";
+import { createPost, getPaginatedPosts, getPosts } from "./apis";
+import { PostInfiniteQuery } from "./types";
 
 export function usePostsQuery() {
   return useQuery<Post[], Error, Post[]>({
@@ -17,5 +23,17 @@ export function useCreatePostMutation() {
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.POSTS] });
     },
+  });
+}
+
+export function usePostsInfiniteQuery() {
+  return useInfiniteQuery<PostInfiniteQuery, Error, Post[], [string], number>({
+    queryKey: [QUERY_KEYS.POSTS],
+    initialPageParam: 1,
+    getNextPageParam: (lastPage, pages) => {
+      return lastPage.totalPages > pages.length ? pages.length + 1 : undefined;
+    },
+    queryFn: ({ pageParam }) => getPaginatedPosts(false, pageParam, LIMIT),
+    select: (data) => data.pages.flatMap((page) => page.data),
   });
 }
